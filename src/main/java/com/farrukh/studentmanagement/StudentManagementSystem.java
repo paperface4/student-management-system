b44ba6daca9a45cb984f5c93c402716fcbd04269
paperface4 <farrukhyasin999@gmail.com>
2026-07-20 23:09:05 +0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/farrukh/studentmanagement/StudentManagementSystem.java
@@ -0,0 +1,13 @@
+package com.farrukh.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystem.class, args);
+	}
+
+}
